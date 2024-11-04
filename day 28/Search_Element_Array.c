641e145525072fc5ba73c186e4c96db95d3e7d2f
Aashish <aashishverma239@gmail.com>
2024-11-04 16:07:25 +0530

    Search element array

---
+++ day 28/Search_Element_Array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,ele,f=0; 
+    
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    
+    int a[n];
+    printf("Enter %d integers: ",n);
+    for(i=0; i<n; i++){
+        scanf("%d",&a[i]);
+    }
+    
+    printf("Element to Serach: ");
+    scanf("%d",&ele);
+    
+    for(i=0; i<n; i++){
+        if(a[i]==ele){
+            printf("Element %d found at index %d\n",ele,i);
+            f=1;
+        }
+    }
+    if(f==0){
+        printf("Element %d is not found in the array");
+    }
+
+    return 0;
+}
